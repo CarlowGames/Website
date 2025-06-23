7dbf08fe5a5b358c87f2ebb8efaacae3e93c9040
CarlowGames <mtgtnm@gmail.com>
2025-06-23 14:59:05 +0100

    Update quartz.config.ts

--- source/quartz.config.ts
+++ source/quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🎲 Carlow Games Club",
+    pageTitle: "Carlow Games Club",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
